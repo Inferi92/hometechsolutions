d3ac4b9bdeb2b5b380dec0196af7a8e32685d2b0
Diogo Jesus <diogojesus92@ua.pt>
2022-02-03 01:38:07 +0000

    d

--- htsolutions/serializers.py
+++ htsolutions/serializers.py
@@ -1,6 +1,3 @@
-from calendar import TUESDAY
-
-from django.forms import ChoiceField
 from .models import Product, Category, Family, SubFamily, Brand, Attribute
 from rest_framework import serializers
 
--- htsolutions/views.py
+++ htsolutions/views.py
@@ -1,7 +1,5 @@
-import errno
 from http.client import responses
 from unicodedata import category
-from black import NothingChanged
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
 import requests
@@ -11,9 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
-from django.core import serializers
 from .serializers import (
     AttributeSerializer,
     BrandSerializer,
